36112a0fea9d26a9a456e7cff848b840587c5b5c
Minigosha <margarita.lopez.romero@gmail.com>
2025-11-04 09:57:41 +0100

    updated dashboard with new graphs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,277 +1,227 @@
 import {
-  DashboardCard,
-  DashboardCardContent,
+    DashboardCard,
+    DashboardCardContent,
 } from "@/components/dashboard/dashboard-card";
 import UserDataCard, {
-  UserDataProps,
+    UserDataProps,
 } from "@/components/dashboard/user-data-card";
 import {
-  Calendar,
-  Coins,
-  CreditCard,
-  UserRoundCheck,
-  Users,
+    Calendar,
+    Newspaper,
+    Coins,
+    UserRoundCheck,
+    Users,
 } from "lucide-react";
-// import { db } from "@/lib/db";
 import { prisma } from "@/lib/prisma";
 import {
-  eachMonthOfInterval,
-  endOfMonth,
-  format,
-  formatDistanceToNow,
-  startOfMonth,
-  subDays,
-  subMonths,
+    eachMonthOfInterval,
+    endOfMonth,
+    format,
+    formatDistanceToNow,
+    startOfMonth,
+    subMonths,
 } from "date-fns";
 import { subYears, startOfYear, endOfYear } from "date-fns";
 import BarChart from "@/components/dashboard/barchart";
 import LineGraph from "@/components/dashboard/line-graph";
-import UserPurchaseCard, {
-  UserPurchaseProps,
-} from "@/components/dashboard/user-purchase-card";
+
 import GoalDataCard from "@/components/dashboard/goal";
 
 export default async function Dashboard() {
-  const currentDate = new Date();
+    const currentDate = new Date();
 
-  //Total subscriptions
-  const subscriptionCount = await prisma.subscription.count();
+    //Total subscriptions
+    const subscriptionCount = await prisma.subscription.count();
 
-  //Premium subscription
-  const premium = await prisma.subscriptionType.findUnique({
-    where: { name: "premium" },
-    include: {
-      subscriptions: {
-        select: { id: true },
-      },
-    },
-  });
-  const totalProfitPremium = premium
-    ? premium.price * premium.subscriptions.length
-    : 0 || 0;
-  console.log("totalProfitPremium", totalProfitPremium);
+    //Premium subscription profit
+    const premiumSubscriptionPrice = 199;
+    const premiumProfit = subscriptionCount * premiumSubscriptionPrice
+    console.log("Premium profit: ", premiumProfit)
 
-  const lastYearStart = startOfYear(subYears(new Date(), 1));
-  const lastYearEnd = endOfYear(subYears(new Date(), 1));
+    //Total articles
+    const totalArticles = await prisma.article.count();
 
-  const subscriptionsLastYear = await prisma.subscription.findMany({
-    where: {
-      createdAt: {
-        gte: lastYearStart,
-        lte: lastYearEnd,
-      },
-    },
-    include: {
-      type: {
-        select: { price: true, name: true },
-      },
-    },
-  });
+    //Subscriptions last year
+    const lastYearStart = startOfYear(subYears(new Date(), 1));
+    const lastYearEnd = endOfYear(subYears(new Date(), 1));
 
-  //Compute total profit
-  const totalProfitLastYear =
-    subscriptionsLastYear.reduce((sum, sub) => sum + sub.type.price, 0) || 0;
+    const subscriptionsLastYear = await prisma.subscription.count({
+        where: {
+            periodStart: {
+                gte: lastYearStart,
+                lte: lastYearEnd,
+            },
+        },
 
-  console.log({ totalProfitLastYear });
 
-  const thirtyDaysAgo = subDays(new Date(), 30);
-  const subscriptionsLast30Days = await prisma.subscription.findMany({
-    where: {
-      createdAt: {
-        gte: thirtyDaysAgo, // createdAt >= 30 days ago
-      },
-    },
-    include: {
-      type: {
-        select: { price: true },
-      },
-    },
-  });
-
-  // Calculate profit last 30 days
-  const totalProfitLast30Days =
-    subscriptionsLast30Days.reduce((sum, sub) => sum + sub.type.price, 0) || 0;
-
-  console.log({ totalProfitLast30Days });
-
-  const recentUsers = await prisma.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    take: 7,
-  });
-  const UserData: UserDataProps[] = recentUsers.map(
-    (account: {
-      name: string;
-      email: string;
-      /*image: unknown;*/ createdAt: Date;
-    }) => ({
-      name: account.name || "Unknown",
-      email: account.email || "Unknown",
-      // image: account.image || "/images/user-icon.jpg",
-      time: formatDistanceToNow(new Date(account.createdAt), {
-        addSuffix: true,
-      }),
-    })
-  );
-
-  const usersThisMonth = await prisma.user.groupBy({
-    by: ["createdAt"],
-    _count: { createdAt: true },
-    orderBy: { createdAt: "asc" },
-  });
-
-  const monthlyUsersData = eachMonthOfInterval({
-    start: startOfMonth(new Date(usersThisMonth[0]?.createdAt || new Date())),
-    end: endOfMonth(currentDate),
-  }).map((month) => {
-    const monthString = format(month, "MMM");
-    const userMonthly = usersThisMonth
-      .filter((user) => format(new Date(user.createdAt), "MMM") === monthString)
-      .reduce((total, user) => total + user._count.createdAt, 0);
-    return { month: monthString, total: userMonthly };
-  });
-  console.log(monthlyUsersData);
-
-  /*** */
-  const sixMonthsAgo = subMonths(currentDate, 6);
-
-  const subscriptionsLast6Months = await prisma.subscription.findMany({
-    where: {
-      createdAt: {
-        gte: sixMonthsAgo,
-        lte: currentDate,
-      },
-    },
-    include: {
-      type: {
-        select: { price: true },
-      },
-    },
-  });
-
-  // Group by month and sum prices
-  const revenueByMonth: Record<string, number> = {};
-
-  subscriptionsLast6Months.forEach((sub) => {
-    const monthKey = format(sub.createdAt, "yyyy-MM");
-    revenueByMonth[monthKey] = (revenueByMonth[monthKey] || 0) + sub.type.price;
-  });
-
-  // Ensure all months exist (even with 0)
-  const monthlyRevenueData = [];
-  for (let i = 5; i >= 0; i--) {
-    const monthDate = subMonths(currentDate, i);
-    const monthKey = format(monthDate, "yyyy-MM");
-    monthlyRevenueData.push({
-      month: format(monthDate, "MMM"),
-      total: revenueByMonth[monthKey] || 0,
     });
-  }
 
-  console.log(monthlyRevenueData);
-  /*** */
+    console.log("subscriptionsLastYear", subscriptionsLastYear)
 
-  //Fetch recent sales
-  const recentSales = await prisma.order.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    take: 7,
-    include: {
-      user: true,
-    },
-  });
 
-  const PurchaseCard: UserPurchaseProps[] = recentSales.map((purchase) => ({
-    name: purchase.user?.name || "Unknown",
-    email: purchase.user?.email || "Unknown",
-    paid: purchase.paid ? "Paid" : "Unpaid",
-    //image: purchase.user?. || "/images/user-icon.jpg",_count
-  }));
+    //Fetch recent users
+    const recentUsers = await prisma.user.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
+        take: 7,
+        select: {
+            name: true,
+            email: true,
+            createdAt: true,
+        },
+    });
+    const UserData: UserDataProps[] = recentUsers.map((account) => ({
+        name: account.name || "Unknown",
+        email: account.email || "Unknown",
+        // image: account.image || "/images/user-icon.jpg",
+        time: formatDistanceToNow(new Date(account.createdAt), {
+            addSuffix: true,
+        }),
+    })
+    );
 
-  const goalAmount = 5000;
-  const goalProgress = (totalProfitPremium / goalAmount) * 100;
+    const usersThisMonth = await prisma.user.groupBy({
+        by: ["createdAt"],
+        _count: { createdAt: true },
+        orderBy: { createdAt: "asc" },
+    });
 
-  return (
-    <div className="flex flex-col gap-5 w-full">
-      <h1 className="text-2xl font-bold text-bold text-center mx-6">
-        Dashboard
-      </h1>
-      <div className="container mx-auto py-8">
+    const monthlyUsersData = eachMonthOfInterval({
+        start: startOfMonth(new Date(usersThisMonth[0]?.createdAt || new Date())),
+        end: endOfMonth(currentDate),
+    }).map((month) => {
+        const monthString = format(month, "MMM");
+        const userMonthly = usersThisMonth
+            .filter((user) => format(new Date(user.createdAt), "MMM") === monthString)
+            .reduce((total, user) => total + user._count.createdAt, 0);
+        return { month: monthString, total: userMonthly };
+
+    });
+    console.log("monthlyUsersData", monthlyUsersData);
+
+    //Subscriptions last 6 months
+    const sixMonthsAgo = subMonths(currentDate, 6);
+    const subscriptionsLast6Months = await prisma.subscription.findMany({
+        where: {
+            periodStart: {
+                gte: sixMonthsAgo,
+                lte: currentDate,
+            },
+        },
+
+        select: {
+            periodStart: true,
+
+        }
+
+    });
+
+
+    const revenueByMonth: Record<string, number> = {};
+
+    subscriptionsLast6Months.forEach((sub) => {
+        if (!sub.periodStart) return;
+        const monthKey = format(sub.periodStart, "yyyy-MM");
+        revenueByMonth[monthKey] = (revenueByMonth[monthKey] || 0) + premiumSubscriptionPrice;
+    });
+
+    const monthlyRevenueData = [];
+    for (let i = 5; i >= 0; i--) {
+        const monthDate = subMonths(currentDate, i);
+        const monthKey = format(monthDate, "yyyy-MM");
+
+        monthlyRevenueData.push({
+            month: format(monthDate, "MMM"),
+            total: revenueByMonth[monthKey] || 0,
+        });
+    }
+
+    console.log("monthlyRevenueData", monthlyRevenueData);
+
+
+
+    //Fetch recent sales
+    const recentSales = await prisma.order.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
+        take: 7,
+        include: {
+            user: true,
+        },
+    });
+
+
+    const goalAmount = 5000;
+    const goalProgress = (premiumProfit / goalAmount) * 100;
+
+    return (
         <div className="flex flex-col gap-5 w-full">
-          <section
-            className="grid w-full grid-cols-1 sm:grid-cols-2 xl:grid-cols-4
+            <h1 className="text-2xl font-bold text-bold text-center mx-6">
+                Dashboard
+            </h1>
+            <div className="container mx-auto py-8">
+                <div className="flex flex-col gap-5 w-full">
+                    <section
+                        className="grid w-full grid-cols-1 sm:grid-cols-2 xl:grid-cols-4
                         gap-4 gap-x-8 transition-all"
-          >
-            <DashboardCard
-              label={"Prenumeranter"}
-              Icon={Users}
-              amount={subscriptionCount}
-              description="Totalt"
-            />
-            <DashboardCard
-              label={"Vinst Premium"}
-              Icon={Coins}
-              amount={`${totalProfitPremium} Kr`}
-              description="Totalt"
-            />
-            <DashboardCard
-              label={"Vinst senaste 30 dagar"}
-              Icon={Calendar}
-              amount={`${totalProfitLast30Days} Kr`}
-              description="Totalt"
-            />
-            <DashboardCard
-              label={"Vinst förra året"}
-              Icon={Calendar}
-              amount={`${totalProfitLastYear} Kr`}
-              description="Totalt"
-            />
-          </section>
-          <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
-            <DashboardCardContent>
-              <section className="flex justify-between gap-2 pb-2">
-                <p>Senaste användare</p>
-                <UserRoundCheck className="h-4 w-4" />
-              </section>
-              {UserData.map((data, index) => (
-                <UserDataCard
-                  key={index}
-                  name={data.name}
-                  //image={data.image}
-                  email={data.email}
-                  time={data.time}
-                ></UserDataCard>
-              ))}
-            </DashboardCardContent>
-            <DashboardCardContent>
-              <section className="flex justify-between gap-2 pb-2">
-                <p>Senaste köp</p>
-                <CreditCard className="h-4 w-4" />
-              </section>
-              {PurchaseCard.map((data, index) => (
-                <UserPurchaseCard
-                  key={index}
-                  name={data.name}
-                  //image={data.image}
-                  email={data.email}
-                  paid={data.paid}
-                ></UserPurchaseCard>
-              ))}
-            </DashboardCardContent>
-          </section>
-          <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
-            <BarChart data={monthlyUsersData}></BarChart>
-            <LineGraph data={monthlyRevenueData} />
-          </section>
-          <GoalDataCard
-            goal={goalAmount}
-            value={totalProfitPremium}
-            bar={goalProgress}
-          />
+                    >
+                        <DashboardCard
+                            label={"Totalt prenumeranter"}
+                            Icon={Users}
+                            amount={subscriptionCount}
+                            description=""
+                        />
+                        <DashboardCard
+                            label={"Prenumeranter förra året"}
+                            Icon={Calendar}
+                            amount={subscriptionsLastYear}
+                            description="Totalt"
+                        />
+                        <DashboardCard
+                            label={"Vinst Premium"}
+                            Icon={Coins}
+                            amount={`${premiumProfit} Kr`}
+                            description="Totalt"
+                        />
+                        <DashboardCard
+                            label={"Antal publicerade artiklar"}
+                            Icon={Newspaper}
+                            amount={totalArticles}
+                            description="Totalt"
+                        />
+
+                    </section>
+                    <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
+                        <DashboardCardContent>
+                            <section className="flex justify-between gap-2 pb-2">
+                                <p>Senaste användare</p>
+                                <UserRoundCheck className="h-4 w-4" />
+                            </section>
+                            {UserData.map((data, index) => (
+                                <UserDataCard
+                                    key={index}
+                                    name={data.name}
+                                    //image={data.image}
+                                    email={data.email}
+                                    time={data.time}
+                                ></UserDataCard>
+                            ))}
+                        </DashboardCardContent>
+
+                    </section>
+                    <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
+                        <BarChart data={monthlyUsersData}></BarChart>
+                        <LineGraph data={monthlyRevenueData} />
+                    </section>
+                    <GoalDataCard
+                        goal={goalAmount}
+                        value={premiumProfit}
+                        bar={goalProgress}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
